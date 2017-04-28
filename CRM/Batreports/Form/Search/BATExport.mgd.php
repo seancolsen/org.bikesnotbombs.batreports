b5c2ace86aa24bcf0ac504afe18b382f7aae399d
Sean Madsen <sean@seanmadsen.com>
2017-04-28 16:02:19 -0600

    civix generate:search BATExport

---
+++ CRM/Batreports/Form/Search/BATExport.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Batreports_Form_Search_BATExport',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'BATExport',
+      'description' => 'BATExport (org.bikesnotbombs.batreports)',
+      'class_name' => 'CRM_Batreports_Form_Search_BATExport',
+    ),
+  ),
+);
